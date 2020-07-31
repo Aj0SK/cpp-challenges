73ffa72bbc815023291a01dcd9a4ac7ff6f6bac6
Aj0SK <andrejkorman@gmail.com>
2020-07-31 15:42:37 +0200

    challenge5: fix ln approximation

--- challenge5/test1.cpp
+++ challenge5/test1.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <cstdint>
+#include <vector>
+#include <cassert>
 #include <iomanip>
 #include <limits>
 #include <type_traits>
+#include <cmath>
 
 using std::cout;
 using std::endl;
+using std::vector;
 
-constexpr int kLnAccuracy = 15;
-constexpr int kSinAccuracy = 15;
+constexpr int kLnAccuracy = 10;
+constexpr int kSinAccuracy = 10;
 constexpr double kPi = 3.14159;
 
 constexpr double get_uniform(uint32_t gen, double a = 0.0, double b = 1.0)
@@ -28,8 +32,20 @@ constexpr uint32_t gen_random(uint32_t prev)
     return curr;
 };
 
-constexpr double ln_fast(double x, double point = 1.0)
+constexpr double ln_fast(double x)
 {
+    if(x<0.1)
+    {
+        double comp = 0;
+        while(x < 1)
+        {
+            x *= 2.0;
+            comp += 0.69314718056;
+        }
+        return ln_fast(x)-comp;
+    }
+
+    double point = 1.0;
     double sum = 0, num = x-point;
 
     for(int i=0; i<kLnAccuracy; ++i)
@@ -118,8 +134,27 @@ void test()
     constexpr double test_inf = 0.0000001;
 
     static_assert(abs_const(ln_fast(0.75) - (-0.28768207245)) < test_inf, "Problem with ln accuracy.");
+
     static_assert(abs_const(sqrt_fast(0.2) - (0.447213595)) < test_inf, "Problem with sqrt accuracy.");
+    static_assert(abs_const(sqrt_fast(3.0) - (1.73205080757)) < test_inf, "Problem with sqrt accuracy.");
+    static_assert(abs_const(sqrt_fast(6.0) - (2.44948974278)) < test_inf, "Problem with sqrt accuracy.");
+
     static_assert(abs_const(sin_fast(0.2) - (0.19866933079)) < test_inf, "Problem with sin accuracy.");
+    static_assert(abs_const(sin_fast(3.0) - (0.14112000806)) < test_inf, "Problem with sin accuracy.");
+
+    for(double x=0.0; x<100.0; x += 0.01)
+    {
+        assert(abs_const(sqrt_fast(x*x) - x) < test_inf);
+    }
+    for(double x=0.0; x<2*kPi; x += 0.001)
+    {
+        assert(abs(sin_fast(x) - sin(x)) < test_inf);
+    }
+
+    for(double x=0.0001; x<1.0; x += 0.00001)
+    {
+        assert(abs(ln_fast(x) - log(x)) < test_inf);
+    }
 }
 
 int main()
